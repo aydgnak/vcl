b4b8327b27ff2b28af4fddd289447f89a63fa749
Aydoğan Ak <aydgnak@gmail.com>
2026-08-21 08:35:36 +0300

    chore(backend): add user module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from './auth'
 import { CoreModule } from './core'
 import { SharedModule } from './shared'
+import { UserModule } from './user'
 
 @Module({
   imports: [
     CoreModule,
     SharedModule,
     AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
