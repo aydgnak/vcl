eb1ab02bb20f3cf6ce2520cb6fb5edbe586793c7
Aydoğan Ak <aydgnak@gmail.com>
2026-08-21 11:01:52 +0300

    feat(backend): enable cookie parsing middleware

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,10 +2,13 @@ import type { ConfigO } from './core/config'
 import { Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(cookieParser())
 
   const configService = app.get(ConfigService<ConfigO, true>)
 
